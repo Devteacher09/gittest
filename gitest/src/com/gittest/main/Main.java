5464ef845a7b6aa7165afa05b11960728a4a5419
teacherdev09 <teacherdev09@gmail.com>
2024-06-10 07:59:39 +0900

    test2commit

--- gitest/src/com/gittest/main/Main.java
+++ gitest/src/com/gittest/main/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("hello java");
 		System.out.println("다른 repository에서 설정");
+		System.out.println("세번째 push");
 	}
 
 }
