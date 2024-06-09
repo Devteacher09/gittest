dda9a693c1661fcf8c51431a13e435751a0924e0
teacherdev09 <teacherdev09@gmail.com>
2024-06-10 08:04:00 +0900

    push받아~

--- gitest/src/com/gittest/main/Main.java
+++ gitest/src/com/gittest/main/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("hello java");
 		System.out.println("다른 repository에서 설정");
 		System.out.println("세번째 push");
+		System.out.println("초대후 push보내기");
 	}
 
 }
